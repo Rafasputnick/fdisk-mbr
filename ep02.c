934d1c5464b183cd44963e1ad3a2da726d929682
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-09-19 13:17:46 -0300

    feat: read binary file and check boot signature

--- ep02.c
+++ ep02.c
@@ -1,6 +1,47 @@
-# include <stdio.h>
+// https://cpl.li/posts/2019-03-12-mbrfat/ 
+// https://wiki.osdev.org/MBR_%28x86%29
 
-int main(){
-   printf("Hello World!\n");
-   return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define READ_BINARY "rb"
+#define BOOT_SIGNATURE_INDEX 510
+
+enum errors {OPENING_FILE, MALLOC, READING_FILE, BOOT_SIGNATURE} error;
+
+int main () {
+  FILE * file = fopen("mbr.bin", READ_BINARY);
+  if(file == NULL) {
+      printf("Error: Opening file");
+      exit(OPENING_FILE);
+   }
+
+  fseek(file , 0 , SEEK_END);
+  uint16_t fileSize = ftell(file);
+  rewind(file);
+
+  char * mbrBuffer = (char*) malloc(sizeof(char)*fileSize);
+  if(mbrBuffer == NULL) {
+      printf("Error: Malloc return null");
+      exit(MALLOC);
+   }
+
+  size_t resultSize = fread(mbrBuffer, 1, fileSize, file);
+  if(resultSize != fileSize) {
+      printf("Error: Reading file");
+      exit(READING_FILE);
+   }
+
+   uint16_t bootSignature = *( (uint16_t *) &mbrBuffer[BOOT_SIGNATURE_INDEX] ); 
+   printf("Boot signature: %04x", bootSignature);
+
+   if(bootSignature != 0xaa55) {
+      printf("Error: File without a valid boot signature");
+      exit(BOOT_SIGNATURE);
+   } 
+
+  fclose(file);
+  free(mbrBuffer);
+  return 0;
 }
